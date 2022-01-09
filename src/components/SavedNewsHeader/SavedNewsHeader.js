176b87dcea94b162ece6a07a8a9f5b6e1b970f82
Frazonar <snip.coloured@gmail.com>
2022-01-09 17:07:58 +0200

    feat: create saved articles header section

--- src/components/SavedNewsHeader/SavedNewsHeader.js
+++ src/components/SavedNewsHeader/SavedNewsHeader.js
@@ -1,8 +1,43 @@
+import React from "react";
+import CurrentUserContext from "../../contexts/CurrentUserContext";
 import "./SavedNewsHeader.css";
 
 function SavedNewsHeader(props) {
+  const currentUser = React.useContext(CurrentUserContext);
+  const userName = currentUser.name.split(" ")[0];
+  const numberOfArticles = props.savedArticles.length | "error";
+  let keywordSet = new Set();
+  props.savedArticles.forEach((article) => {
+    keywordSet.add(article.keyword);
+  });
+  const keywords = Array.from(keywordSet);
+
+  let keywordString = "";
+  switch (keywords.length) {
+    case 0:
+      keywordString = "none";
+      break;
+
+    case 1:
+      keywordString = keywords[0];
+      break;
+
+    case 2:
+      keywordString = `${keywords[0]}, and ${keywords[1]}`;
+      break;
+
+    default:
+      keywordString = `${keywords[0]}, ${keywords[1]}, and ${keywords.length - 2} other`;
+  }
+
   return (<section className="SavedNewsHeader">
-    <div className="SavedNewsHeader__container"></div>
+    <div className="SavedNewsHeader__container">
+      <p className="SavedNewsHeader__title">Saved articles</p>
+      <h2 className="SavedNewsHeader__subtitle">{userName}, you have {numberOfArticles} saved articles</h2>
+      <p className="SavedNewsHeader__keywords">
+        By keywords: <span className="SavedNewsHeader__keyword-list">{keywordString}</span>
+      </p>
+    </div>
   </section>);
 }
 
