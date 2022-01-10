7e59055a2894656eb1f0bd8cfd5a91ad9ad66de1
Frazonar <snip.coloured@gmail.com>
2022-01-10 13:01:54 +0200

    feat: make about section responsive in mobile resolutions

--- src/components/AboutSection/AboutSection.js
+++ src/components/AboutSection/AboutSection.js
@@ -9,7 +9,7 @@ function AboutSection() {
         <ul className="AboutSection__text-container">
           <li><h2 className="AboutSection__title">About the author</h2></li>
           <li><p className="AboutSection__paragraph">This block describes the project author. Here you should indicate your name, what you do, and which development technologies you know.</p></li>
-          <li><p className="AboutSection__paragraph">You can also talk about your experience with Practicum, what you learned there, and how you can help potential customers.</p></li>
+          <li><p className="AboutSection__paragraph AboutSection__paragraph_last">You can also talk about your experience with Practicum, what you learned there, and how you can help potential customers.</p></li>
         </ul>
       </div>
     </section>
